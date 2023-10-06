03d759ac6a201e5d58a912400d50ad4d3031dddc
Terenty Rezman <terenty.rezman@mail.ru>
2023-10-06 09:45:19 +0100

    rasp flask full controll

--- rasp_server/control_server.py
+++ rasp_server/control_server.py
@@ -8,7 +8,8 @@ from apscheduler.schedulers.background import BackgroundScheduler
 
 
 BAUD = 115200
-medium.USE_WEBRTC = True
+medium.USE_WEBRTC = True # use webrtc frontend for camera
+
 
 # connect to stm32 via UART
 ser = serial.Serial('/dev/ttyAMA0', BAUD, write_timeout=0)  # open serial port
@@ -50,10 +51,10 @@ def joyUpdated(joy, client_sid):
     
     last_controll_time = time.time()
 
-    joy_left_x = abs(int(joy["joy_left_x"]))
-    joy_left_y = abs(int(joy["joy_left_y"]))
-    joy_right_x = abs(int(joy["joy_right_x"]))
-    joy_right_y = abs(int(joy["joy_right_y"]))
+    joy_left_x = int(joy["joy_left_x"])
+    joy_left_y = int(joy["joy_left_y"])
+    joy_right_x = int(joy["joy_right_x"])
+    joy_right_y = int(joy["joy_right_y"])
 
     joy_left_x = int(joy_left_x / 100 * 255)
     joy_left_y = int(joy_left_y / 100 * 255)
@@ -65,18 +66,24 @@ def joyUpdated(joy, client_sid):
     joy_right_x = min(255, joy_right_x)
     joy_right_y = min(255, joy_right_y)
 
-    joy_left_x_negative = 1 if int(joy["joy_left_x"]) < 0 else 2
-    joy_left_y_negative = 1 if int(joy["joy_left_y"]) < 0 else 2
-    joy_right_x_negative = 1 if int(joy["joy_right_x"]) < 0 else 2
-    joy_right_y_negative = 1 if int(joy["joy_right_y"]) < 0 else 2
+    e1 = joy_left_y + joy_left_x + joy_right_y + joy_right_x 
+    e2 = joy_left_y - joy_left_x + joy_right_y - joy_right_x
+    e3 = joy_left_y - joy_left_x + joy_right_y + joy_right_x
+    e4 = joy_left_y + joy_left_x + joy_right_y - joy_right_x
+
+    e1_direction = 1 if int(e1) > 0 else 2
+    e2_direction = 1 if int(e2) < 0 else 2
+    e3_direction = 1 if int(e3) < 0 else 2
+    e4_direction = 1 if int(e4) > 0 else 2
+
+    e1 = min(255, abs(e1))
+    e2 = min(255, abs(e2))
+    e3 = min(255, abs(e3))
+    e4 = min(255, abs(e4))
 
     arr = [
-        joy_left_x, joy_left_y, joy_right_x, joy_right_y, joy_left_x_negative, joy_left_y_negative, joy_right_x_negative, joy_right_y_negative
+        e1, e2, e3, e4, e1_direction, e2_direction, e3_direction, e4_direction
     ]
-
-    # arr = [
-    #     201, 202, 203, 204, 1, 2, 3, 4
-    # ]
 
     write_uart(arr)
 
@@ -90,6 +97,8 @@ def reset_controlling_client():
     if time.time() - last_controll_time > 1:
         controlling_client = None
         print("control client disconnect")
+
+        # stop the car
         write_uart([0, 0, 0, 0, 0, 0, 0, 0])
 
 
